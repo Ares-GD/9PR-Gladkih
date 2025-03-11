5dd2a27a4633f85faec8bd72d8fd9695a9abe531
Ares-GladkihD <ares-gd@yandex.ru>
2025-03-11 21:33:31 +0500

    Переделан код тестирования и сам auth, а также файл get_api

--- auth.permaviat.ru/index.php
+++ auth.permaviat.ru/index.php
@@ -1,47 +1,35 @@
 <?php
-include("connect_datebase.php");
+    include("settings/connect.php");
+  
+    if (!isset($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_USER'])) { header('HTTP/1.0 403 Forbidden'); exit; }
+    if (!isset($_SERVER['PHP_AUTH_PW']) || empty($_SERVER['PHP_AUTH_PW'])) { header('HTTP/1.0 403 Forbidden'); exit; }
 
-if (!isset($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_USER'])) {
-    header('HTTP/1.0 403 Forbidden');
-    exit;
-}
+    $login = $_SERVER['PHP_AUTH_USER'];
+    $password = $_SERVER['PHP_AUTH_PW'];
 
-if (!isset($_SERVER['PHP_AUTH_PW']) || empty($_SERVER['PHP_AUTH_PW'])) {
-    header('HTTP/1.0 403 Forbidden');
-    exit;
-}
+    $query_user = $mysqli->query("SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
+    if ($read_user = $query_user->fetch_assoc()) {
+        $header = ["typ" => "JWT", "alg" => "sha256"];
+        $payload = [
+            "userId" => $read_user['id'], 
+            "role" => $read_user['roll']  
+        ];
+        $SECRET_KEY = 'cAtwa1kkEy'; 
 
-$login = $_SERVER['PHP_AUTH_USER'];
-$password = $_SERVER['PHP_AUTH_PW'];
+        $base64Header = base64_encode(json_encode($header)); 
+        $base64Payload = base64_encode(json_encode($payload)); 
+        
+        $unsignedToken = $base64Header . "." .$base64Payload; 
+        $signature = hash_hmac('sha256', $unsignedToken, $SECRET_KEY, true);
 
-$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
+        $base64Signature = base64_encode($signature); 
+       
+        $token =  $base64Header . '.'. $base64Payload . '.' . $base64Signature;
 
-if ($read_user = $query_user->fetch_assoc()) {
-    // Формирование JWT токена
-    $header = [
-        "typ" => "JWT",
-        "alg" => "sha256"
-    ];
-
-    $payload = [
-        "userId" => $read_user['id'],
-        "role" => $read_user['roll']
-    ];
-
-    $SECRET_KEY = 'cAtwa1kkEy';
-
-    $unsignedToken = base64_encode(json_encode($header)) . '.' .
-                     base64_encode(json_encode($payload));
-
-    $signature = hash_hmac($header['alg'], $unsignedToken, $SECRET_KEY);
-
-    $token = base64_encode(json_encode($header)) . '.' .
-             base64_encode(json_encode($payload)) . '.' .
-             base64_encode($signature);
-
-    header("token: " . $token);
-    header('HTTP/1.0 200 OK');
-} else {
-    header('HTTP/1.0 401 Unauthorized');
-}
+       http_response_code(200); 
+        echo json_encode(['token' => $token]);
+        } else {
+        http_response_code(401); 
+        echo json_encode(['error' => 'Invalid credentials']); 
+            }
 ?>
